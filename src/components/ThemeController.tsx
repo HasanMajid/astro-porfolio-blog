3218e14e5568d7b5e97639d6c96d148b629940ed
Hasan Majid <hasanmajid3.14@hotmail.com>
2024-08-02 00:25:34 -0400

    added and converted to DaisyUI

---
+++ src/components/ThemeController.tsx
@@ -0,0 +1,93 @@
+function ThemeController() {
+    const setTheme = (theme: string) => {
+        document.documentElement.setAttribute('data-theme', theme)
+        window.localStorage.setItem('data-theme', theme)
+    }
+    return (
+        <div class="dropdown mb-72">
+            <div
+                tabindex="0"
+                role="button"
+                class="btn m-1 shadow-md shadow-gray-500/50"
+            >
+                Theme
+                <svg
+                    width="12px"
+                    height="12px"
+                    class="inline-block h-2 w-2 fill-current opacity-60"
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 2048 2048"
+                >
+                    <path d="M1799 349l242 241-1017 1017L7 590l242-241 775 775 775-775z"></path>
+                </svg>
+            </div>
+            <ul
+                tabindex="0"
+                class="dropdown-content bg-base-300 rounded-box z-[1] w-52 p-2 shadow-2xl"
+            >
+                <li>
+                    <input
+                        type="radio"
+                        name="theme-dropdown"
+                        class="theme-controller btn btn-sm btn-block btn-ghost justify-start"
+                        aria-label="Light"
+                        value="light"
+                        onClick={() => setTheme('light')}
+                    />
+                </li>
+                <li>
+                    <input
+                        type="radio"
+                        name="theme-dropdown"
+                        class="theme-controller btn btn-sm btn-block btn-ghost justify-start"
+                        aria-label="Dark"
+                        value="dark"
+                        onClick={() => setTheme('dark')}
+                    />
+                </li>
+                <li>
+                    <input
+                        type="radio"
+                        name="theme-dropdown"
+                        class="theme-controller btn btn-sm btn-block btn-ghost justify-start"
+                        aria-label="Forest"
+                        value="forest"
+                        onClick={() => setTheme('forest')}
+                    />
+                </li>
+                <li>
+                    <input
+                        type="radio"
+                        name="theme-dropdown"
+                        class="theme-controller btn btn-sm btn-block btn-ghost justify-start"
+                        aria-label="Cyberpunk"
+                        value="cyberpunk"
+                        onClick={() => setTheme('cyberpunk')}
+                    />
+                </li>
+                <li>
+                    <input
+                        type="radio"
+                        name="theme-dropdown"
+                        class="theme-controller btn btn-sm btn-block btn-ghost justify-start"
+                        aria-label="Valentine"
+                        value="valentine"
+                        onClick={() => setTheme('valentine')}
+                    />
+                </li>
+                <li>
+                    <input
+                        type="radio"
+                        name="theme-dropdown"
+                        class="theme-controller btn btn-sm btn-block btn-ghost justify-start"
+                        aria-label="Aqua"
+                        value="aqua"
+                        onClick={() => setTheme('aqua')}
+                    />
+                </li>
+            </ul>
+        </div>
+    )
+}
+
+export default ThemeController
